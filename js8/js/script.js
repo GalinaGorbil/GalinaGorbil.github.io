e1853371a6648076729868137b0ec8e244683302
Galina Gorbil <galina_gorbil@ukr.net>
2017-08-10 20:50:16 +0300

    add first form with animation

---
+++ js8/js/script.js
@@ -0,0 +1,48 @@
+$(function () {
+
+    var $firstName = $('.firstname'),
+    $lastName = $('.lastname'),
+    $address = $('.address'),
+    $prompt_block = $('.prompt1, .prompt2, .prompt3'),
+    $prompt1 = $('.prompt1'),
+    $prompt2 = $('.prompt2'),
+    $prompt3 = $('.prompt3'),
+    $button = $('[type="button"]');
+
+
+    $firstName.hover( function () {
+        $prompt1.css('opacity', '100')
+
+    }).mouseout( function () {
+        $prompt1.animate({
+            opacity: '0'
+        }, 1000);
+    });
+
+
+    $lastName.hover( function () {
+        $prompt2.css('opacity', '100')
+
+    }).mouseout( function () {
+        $prompt2.animate({
+            opacity: '0'
+        }, 1000);
+    });
+
+
+    $address.hover( function () {
+        $prompt3.css('opacity', '100');
+
+    }).mouseout( function () {
+        $prompt3.animate({
+            opacity: '0'
+        }, 1000);
+    });
+
+
+    $button.on('click', function () {
+        $prompt_block.css('opacity', '100');
+    });
+
+
+});
